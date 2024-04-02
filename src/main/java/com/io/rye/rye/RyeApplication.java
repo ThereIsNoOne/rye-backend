a2f58f175e0b928f7e57f41edf1bf0a47964c10f
= <szymon.s.lasota@gmail.com>
2024-04-02 19:09:41 +0200

    Initial commit

---
+++ src/main/java/com/io/rye/rye/RyeApplication.java
@@ -0,0 +1,13 @@
+package com.io.rye.rye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RyeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RyeApplication.class, args);
+    }
+
+}
